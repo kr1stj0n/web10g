bdb61079cb81d47644743eb2dbd5ef68ca143aef
kr1stj0n <kr1stj0n@pm.me>
2021-05-06 11:00:06 +0200

    fixed build tools

--- web10g-kis-0.13-5.4/tools/lgc_shq/build_mod.sh
+++ web10g-kis-0.13-5.4/tools/lgc_shq/build_mod.sh
@@ -13,7 +13,7 @@ build_modules() {
          sudo depmod -a;
          sudo cp /lib/modules/5.4.0/extra/sch_shq.ko /lib/modules/5.4.0/kernel/net/sched/;
          sudo depmod -a;
-         cp Makefile.shq.init ../../net/sched/Makefile;
+         cp Makefile.sched.init ../../net/sched/Makefile;
          sudo modprobe sch_shq"
 
     eval $CMD
@@ -25,7 +25,7 @@ build_modules() {
          sudo depmod -a;
          sudo cp /lib/modules/5.4.0/extra/sch_red.ko /lib/modules/5.4.0/kernel/net/sched/;
          sudo depmod -a;
-         cp Makefile.shq.init ../../net/sched/Makefile;
+         cp Makefile.sched.init ../../net/sched/Makefile;
          sudo modprobe sch_red"
 
     eval $CMD
@@ -33,16 +33,11 @@ build_modules() {
     # TCP_LGC
     CMD="sudo rmmod -f tcp_lgc;
          cp Makefile.lgc ../../net/ipv4/Makefile;
-         cd ../../;
-         sudo modules_prepare;
-         sudo make M=../../net/ipv4/ clean;
-         sudo make M=../../net/ipv4/ modules;
-         sudo make M=../../net/ipv4/ modules_install;
+         cd ../../ && sudo modules_prepare && sudo make M=net/ipv4/ clean && sudo make M=net/ipv4/ modules && sudo make M=net/ipv4/ modules_install;
          sudo depmod -a;
          sudo cp /lib/modules/5.4.0/extra/tcp_lgc.ko /lib/modules/5.4.0/kernel/net/ipv4/;
          sudo depmod -a;
-         cd tools/lgc_shq;
-         cp Makefile.lgc.init ../../net/ipv4/Makefile;
+         cp Makefile.ipv4.init net/ipv4/Makefile;
          sudo modprobe tcp_lgc"
 
     # eval $CMD
