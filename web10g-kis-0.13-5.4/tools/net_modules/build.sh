e9a3e634769a88706a219b2c262c6deaa3654b89
kr1stj0n <kr1stj0n@pm.me>
2021-06-02 12:51:45 +0200

    small fixes

--- web10g-kis-0.13-5.4/tools/net_modules/build.sh
+++ web10g-kis-0.13-5.4/tools/net_modules/build.sh
@@ -4,8 +4,16 @@
 # Written by: Kr1stj0n C1k0 <kristjoc@ifi.uio.no>
 #
 
-build_modules() {
+build_stack() {
 
+    CMD="cd ../../ && sudo make modules_prepare && sudo make -j32 && sudo make -j32 modules_install && sudo make -j32 headers_install;
+         sudo depmod -a;
+         sudo find /lib/modules/5.4.0/ -name *.ko -exec strip --strip-unneeded {} +;
+         sudo rm -rf /boot/*5.4.0*;
+         sudo make -j32 install"
+}
+
+build_modules() {
     # SCH_SHQ
     CMD="sudo rmmod -f sch_shq;
          cp Makefile.shq ../../net/sched/Makefile;
@@ -42,6 +50,18 @@ build_modules() {
 
     #eval $CMD
 
+    # TCP_DCTCP
+    CMD="sudo rmmod -f tcp_dctcp;
+         cp Makefile.dctcp ../../net/ipv4/Makefile;
+         cd ../../ && sudo make modules_prepare && sudo make M=net/ipv4/ clean && sudo make M=net/ipv4/ modules && sudo make M=net/ipv4/ modules_install;
+         sudo depmod -a;
+         sudo cp /lib/modules/5.4.0/extra/tcp_dctcp.ko /lib/modules/5.4.0/kernel/net/ipv4/;
+         sudo depmod -a;
+         cp tools/net_modules/Makefile.ipv4.init net/ipv4/Makefile;
+         sudo modprobe tcp_dctcp"
+
+    #eval $CMD
 }
 
-build_modules
+#build_stack
+#build_modules
