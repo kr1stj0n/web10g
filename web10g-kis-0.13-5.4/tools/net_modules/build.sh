1a9b8191a6ca3af1fc837ce538997ee2af44ef51
kr1stj0n <kr1stj0n@pm.me>
2021-06-14 12:24:02 +0200

    fix build process

--- web10g-kis-0.13-5.4/tools/net_modules/build.sh
+++ web10g-kis-0.13-5.4/tools/net_modules/build.sh
@@ -6,7 +6,7 @@
 
 build_stack() {
 
-    CMD="cd ../../ && sudo make modules_prepare && sudo make -j32 && sudo make -j32 modules_install && sudo make -j32 headers_install;
+    CMD="cd ../../ && make modules_prepare && make -j32 && sudo make -j32 modules_install && sudo make -j32 headers_install;
          sudo depmod -a;
          sudo find /lib/modules/5.4.0/ -name *.ko -exec strip --strip-unneeded {} +;
          sudo rm -rf /boot/*5.4.0*;
@@ -19,7 +19,7 @@ build_modules() {
     # SCH_SHQ
     CMD="sudo rmmod -f sch_shq;
          cp Makefile.shq ../../net/sched/Makefile;
-         cd ../../ && sudo make modules_prepare && sudo make M=net/sched/ clean && sudo make M=net/sched/ modules && sudo make M=net/sched/ modules_install;
+         cd ../../ && make modules_prepare && make M=net/sched/ clean && make M=net/sched/ modules && sudo make M=net/sched/ modules_install;
          sudo depmod -a;
          sudo cp /lib/modules/5.4.0/extra/sch_shq.ko /lib/modules/5.4.0/kernel/net/sched/;
          sudo depmod -a;
@@ -31,7 +31,7 @@ build_modules() {
     # SCH_RED
     CMD="sudo rmmod -f sch_red;
          cp Makefile.red ../../net/sched/Makefile;
-         cd ../../ && sudo make modules_prepare && sudo make M=net/sched/ clean && sudo make M=net/sched/ modules && sudo make M=net/sched/ modules_install;
+         cd ../../ && make modules_prepare && make M=net/sched/ clean && make M=net/sched/ modules && sudo make M=net/sched/ modules_install;
          sudo depmod -a;
          sudo cp /lib/modules/5.4.0/extra/sch_red.ko /lib/modules/5.4.0/kernel/net/sched/;
          sudo depmod -a;
@@ -43,7 +43,7 @@ build_modules() {
     # TCP_LGC
     CMD="sudo rmmod -f tcp_lgc;
          cp Makefile.lgc ../../net/ipv4/Makefile;
-         cd ../../ && sudo make modules_prepare && sudo make M=net/ipv4/ clean && sudo make M=net/ipv4/ modules && sudo make M=net/ipv4/ modules_install;
+         cd ../../ && make modules_prepare && make M=net/ipv4/ clean && make M=net/ipv4/ modules && sudo make M=net/ipv4/ modules_install;
          sudo depmod -a;
          sudo cp /lib/modules/5.4.0/extra/tcp_lgc.ko /lib/modules/5.4.0/kernel/net/ipv4/;
          sudo depmod -a;
@@ -55,7 +55,7 @@ build_modules() {
     # TCP_DCTCP
     CMD="sudo rmmod -f tcp_dctcp;
          cp Makefile.dctcp ../../net/ipv4/Makefile;
-         cd ../../ && sudo make modules_prepare && sudo make M=net/ipv4/ clean && sudo make M=net/ipv4/ modules && sudo make M=net/ipv4/ modules_install;
+         cd ../../ && make modules_prepare && make M=net/ipv4/ clean && make M=net/ipv4/ modules && sudo make M=net/ipv4/ modules_install;
          sudo depmod -a;
          sudo cp /lib/modules/5.4.0/extra/tcp_dctcp.ko /lib/modules/5.4.0/kernel/net/ipv4/;
          sudo depmod -a;
