7341b6fae38947cff647fc92c42743e460365f6a
kr1stj0n <kr1stj0n@pm.me>
2022-03-03 11:42:48 +0100

    towards a better hull

--- web10g-kis-0.13-5.4/net/sched/sch_hull.c
+++ web10g-kis-0.13-5.4/net/sched/sch_hull.c
@@ -1,15 +1,15 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-/*
- * net/sched/sch_hull.c HULL
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright (C) University of Oslo, Norway, 2021.
  *
- * Author:	Kristjon Ciko
+ * Author: Kr1stj0n C1k0 <kristjoc@ifi.uio.no>
+ *
+ * References:
+ * TODO
  */
 
-#include "linux/pkt_sched.h"
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
-#include <linux/string.h>
 #include <linux/errno.h>
 #include <linux/skbuff.h>
 #include <net/netlink.h>
@@ -20,9 +20,15 @@
 
 /* parameters used */
 struct hull_params {
-	u32 limit;	/* Maximal length of backlog: bytes */
-	u32 drate; 	/* drain rate of PQ */
-	u32 markth;	/* ECN marking threshold */
+	u32 limit;		/* number of packets that can be enqueued */
+	u32 drate;		/* PQ drain rate in bpms */
+	u32 markth;		/* ECN marking threshold */
+};
+
+/* variables used */
+struct hull_vars {
+	u64 counter;		/* PQ counter */
+	psched_time_t last;	/* last time a packet was enqueued */
 };
 
 /* statistics gathering */
@@ -30,181 +36,87 @@ struct hull_stats {
 	u32 avg_rate;		/* current average rate */
 	u64 qdelay;		/* current queuing delay */
 	u32 packets_in;		/* total number of packets enqueued */
-	u32 dropped;		/* packets dropped due to hull_action */
+	u32 dropped;		/* packets dropped due to shq_action */
 	u32 overlimit;		/* dropped due to lack of space in queue */
 	u16 maxq;		/* maximum queue size ever seen */
 	u32 ecn_mark;		/* packets marked with ECN */
 };
 
-/* variables used */
-struct hull_vars {
-	u64 last;	/* time when last packet was dequeued */
-	u64 freq;	/* dequeuing frequency */
-	u32 counter;	/* PQ counter */
-	u32 mtu;	/* MTU of interface */
-};
-
+/* private data for the Qdisc */
 struct hull_sched_data {
-	struct hull_params params;	/* HULL parameters */
-	struct hull_vars vars;		/* HULL variables */
-	struct hull_stats stats;	/* HULL statistics */
-	struct qdisc_watchdog watchdog;	/* Watchdog timer */
-	struct Qdisc	*qdisc;		/* Inner qdisc, default - bfifo queue */
+	struct hull_params params;
+	struct hull_vars vars;
+	struct hull_stats stats;
+	struct Qdisc *sch;
 };
 
-static void hull_params_init(struct hull_sched_data *q)
+static void hull_params_init(struct hull_params *params)
 {
-	q->params.limit  = 1000U;		/* default of 1000 packets */
-	q->params.drate  = 12500000U;		/* default 100Mbps */
-	q->params.markth = 1514U;		/* default 1 pkt */
+	params->limit  = 1500000U;	/* default of 1000 packets */
+	params->drate  = 12500U;	/* 100Mbps draining rate */
+	params->markth = 1500U;		/* ECN marking threshold of 1p */
 }
 
-static void hull_vars_init(struct hull_sched_data *q)
+static void hull_vars_init(struct hull_vars *vars)
 {
-	q->vars.last	= ktime_get_ns();  /* last time a packet was dequeued */
-	q->vars.freq	= 120000ULL;       /* draining rate frequency in ns */
-	q->vars.counter = 0U;
-	q->vars.mtu	= 1514U;
+	vars->counter = 0ULL;
+	vars->last = psched_get_time();
 }
 
-/* Calculates the frequency in ns of dequeueing a packet based on drain rate
- */
-static inline u64 psched_ns_freq(u32 drate, u32 mtu)
-{
-	u64 div = (u64)(1ULL * mtu * NSEC_PER_SEC);
-
-	do_div(div, drate);
-
-	return div;
-}
-
-/* GSO packet is too big, segment it so that hull can transmit
- * each segment in time
- */
-static int hull_segment(struct sk_buff *skb, struct Qdisc *sch,
-			struct sk_buff **to_free)
+static int hull_qdisc_enqueue(struct sk_buff *skb, struct Qdisc *sch,
+			      struct sk_buff **to_free)
 {
 	struct hull_sched_data *q = qdisc_priv(sch);
-	struct sk_buff *segs, *nskb;
-	netdev_features_t features = netif_skb_features(skb);
-	unsigned int len = 0, prev_len = qdisc_pkt_len(skb);
-	int ret, nb;
+	psched_tdiff_t delta;
+	u64 max_bytes = (u64)q->params.drate;
 
-	segs = skb_gso_segment(skb, features & ~NETIF_F_GSO_MASK);
-
-	if (IS_ERR_OR_NULL(segs))
-		return qdisc_drop(skb, sch, to_free);
-
-	nb = 0;
-	while (segs) {
-		nskb = segs->next;
-		skb_mark_not_on_list(segs);
-		qdisc_skb_cb(segs)->pkt_len = segs->len;
-		len += segs->len;
-		ret = qdisc_enqueue(segs, q->qdisc, to_free);
-		if (ret != NET_XMIT_SUCCESS) {
-			if (net_xmit_drop_count(ret))
-				qdisc_qstats_drop(sch);
-		} else {
-			nb++;
-		}
-		segs = nskb;
-	}
-	sch->q.qlen += nb;
-	if (nb > 1)
-		qdisc_tree_reduce_backlog(sch, 1 - nb, prev_len - len);
-	consume_skb(skb);
-	return nb > 0 ? NET_XMIT_SUCCESS : NET_XMIT_DROP;
-}
-
-static int hull_enqueue(struct sk_buff *skb, struct Qdisc *sch,
-		        struct sk_buff **to_free)
-{
-	struct hull_sched_data *q = qdisc_priv(sch);
-	unsigned int len;
-	int ret;
-
-	if (qdisc_pkt_len(skb) > q->vars.mtu) {
-		if (skb_is_gso(skb) &&
-		    skb_gso_validate_mac_len(skb, q->vars.mtu))
-			return hull_segment(skb, sch, to_free);
-		return qdisc_drop(skb, sch, to_free);
+	if (unlikely(qdisc_qlen(sch) >= sch->limit)) {
+		q->stats.overlimit++;
+		goto out;
 	}
 
-	len = qdisc_pkt_len(skb);
-	if (q->vars.counter + len > q->params.markth) {
-		if (INET_ECN_set_ce(skb)) {
-			/* If packet is ecn capable, mark it with a prob. */
+	/* Calculate the maximum number of bytes that could have been
+	 * arrived during the last interval
+	 */
+	delta = psched_get_time() - q->vars.last;
+	max_bytes *= PSCHED_TICKS2NS(delta);
+	do_div(max_bytes, NSEC_PER_MSEC);
+
+	if (q->vars.counter > max_bytes)
+		q->vars.counter -= max_bytes;
+	else
+		q->vars.counter = 0ULL;
+
+	/* Increase the counter by the size of the packet */
+	q->vars.counter += qdisc_pkt_len(skb);
+
+	/* if HULL counter > markth, then HULL will mark the packet. */
+	if (q->params.markth < (u32)q->vars.counter) {
+		if (INET_ECN_set_ce(skb))
+			/* If packet is ecn capable, mark it with ECN flag. */
 			q->stats.ecn_mark++;
-		}
 	}
-
-	/* Timestamp the packet in order to calculate
-	 * * the queuing delay in the dequeue process.
-	 * */
-	__net_timestamp(skb);
-
-	ret = qdisc_enqueue(skb, q->qdisc, to_free);
-	if (ret != NET_XMIT_SUCCESS) {
-		if (net_xmit_drop_count(ret))
-			qdisc_qstats_drop(sch);
-		return ret;
-	}
-
-	sch->qstats.backlog += len;
-	sch->q.qlen++;
+	/* we can enqueue the packet now */
+	q->stats.packets_in++;
 	if (qdisc_qlen(sch) > q->stats.maxq)
 		q->stats.maxq = qdisc_qlen(sch);
 
-	q->vars.counter += len;
-	q->stats.packets_in++;
+	/* Timestamp the packet in order to calculate
+	 * the queuing delay in the dequeue process.
+	 */
+	__net_timestamp(skb);
+	q->vars.last = psched_get_time();
+	return qdisc_enqueue_tail(skb, sch);
 
-	return NET_XMIT_SUCCESS;
-}
-
-static struct sk_buff *hull_dequeue(struct Qdisc *sch)
-{
-	struct hull_sched_data *q = qdisc_priv(sch);
-	struct sk_buff *skb;
-	unsigned int len;
-	u64 qdelay = 0ULL;
-
-	skb = q->qdisc->ops->peek(q->qdisc);
-
-	if (skb) {
-		if (ktime_get_ns() >= q->vars.last + q->vars.freq) {
-			skb = qdisc_dequeue_peeked(q->qdisc);
-			if (unlikely(!skb))
-				return NULL;
-
-			q->vars.last = ktime_get_ns();
-			qdisc_qstats_backlog_dec(sch, skb);
-			len = qdisc_pkt_len(skb);
-			if (q->vars.counter > len)
-				q->vars.counter -= len;
-			else
-				q->vars.counter = 0U;
-			sch->q.qlen--;
-			qdisc_bstats_update(sch, skb);
-			/* >> 10 is approx /1000 */
-			qdelay = ((__force __u64)(ktime_get_real_ns() -
-					ktime_to_ns(skb_get_ktime(skb)))) >> 10;
-			q->stats.qdelay = qdelay;
-
-			return skb;
-		}
-
-		qdisc_watchdog_schedule_ns(&q->watchdog,
-					   q->vars.last + q->vars.freq);
-		qdisc_qstats_overlimit(sch);
-	}
-	return NULL;
+out:
+	q->stats.dropped++;
+	return qdisc_drop(skb, sch, to_free);
 }
 
 static const struct nla_policy hull_policy[TCA_HULL_MAX + 1] = {
-	[TCA_HULL_LIMIT]  = { .type = NLA_U32 },
-	[TCA_HULL_DRATE]  = { .type = NLA_U32 },
-	[TCA_HULL_MARKTH] = { .type = NLA_U32 },
+	[TCA_HULL_LIMIT]  = {.type = NLA_U32},
+	[TCA_HULL_DRATE]  = {.type = NLA_U32},
+	[TCA_HULL_MARKTH] = {.type = NLA_U32},
 };
 
 static int hull_change(struct Qdisc *sch, struct nlattr *opt,
@@ -212,85 +124,78 @@ static int hull_change(struct Qdisc *sch, struct nlattr *opt,
 {
 	struct hull_sched_data *q = qdisc_priv(sch);
 	struct nlattr *tb[TCA_HULL_MAX + 1];
-	struct Qdisc *child = NULL;
+	u32 qlen, dropped = 0U;
 	int err;
+
+	if (!opt)
+		return -EINVAL;
 
 	err = nla_parse_nested_deprecated(tb, TCA_HULL_MAX, opt, hull_policy,
 			NULL);
 	if (err < 0)
 		return err;
 
-	err = -EINVAL;
-	if (tb[TCA_HULL_LIMIT] == NULL)
-		goto done;
-	q->params.limit = nla_get_u32(tb[TCA_HULL_LIMIT]);
-	sch->limit = q->params.limit;
+	sch_tree_lock(sch);
 
-	if (tb[TCA_HULL_DRATE]) {
-		q->params.drate = nla_get_u32(tb[TCA_HULL_DRATE]);
-		q->vars.freq = psched_ns_freq(q->params.drate, q->vars.mtu);
+	if (tb[TCA_HULL_LIMIT]) {
+		u32 limit = nla_get_u32(tb[TCA_HULL_LIMIT]);
+
+		q->params.limit = limit;
+		sch->limit = limit;
 	}
+
+	if (tb[TCA_HULL_DRATE])
+		q->params.drate = nla_get_u32(tb[TCA_HULL_DRATE]) / MSEC_PER_SEC;
 
 	if (tb[TCA_HULL_MARKTH])
 		q->params.markth = nla_get_u32(tb[TCA_HULL_MARKTH]);
 
-	if (q->qdisc != &noop_qdisc) {
-		err = fifo_set_limit(q->qdisc, q->params.limit);
-		if (err)
-			goto done;
-	} else if (q->params.limit > 0) {
-		child = fifo_create_dflt(sch, &bfifo_qdisc_ops, q->params.limit,
-					 extack);
-		if (IS_ERR(child)) {
-			err = PTR_ERR(child);
-			goto done;
-		}
+	/* Drop excess packets if new limit is lower */
+	qlen = sch->q.qlen;
+	while (sch->q.qlen > sch->limit) {
+		struct sk_buff *skb = __qdisc_dequeue_head(&sch->q);
 
-		/* child is fifo, no need to check for noop_qdisc */
-		qdisc_hash_add(child, true);
+		dropped += qdisc_pkt_len(skb);
+		qdisc_qstats_backlog_dec(sch, skb);
+		rtnl_qdisc_drop(skb, sch);
 	}
-	sch_tree_lock(sch);
-	if (child) {
-		qdisc_tree_flush_backlog(q->qdisc);
-		qdisc_put(q->qdisc);
-		q->qdisc = child;
-	}
+	qdisc_tree_reduce_backlog(sch, qlen - sch->q.qlen, dropped);
+
 	sch_tree_unlock(sch);
-	err = 0;
-done:
-	return err;
+	return 0;
 }
 
 static int hull_init(struct Qdisc *sch, struct nlattr *opt,
 		     struct netlink_ext_ack *extack)
 {
 	struct hull_sched_data *q = qdisc_priv(sch);
+	int err;
 
-	qdisc_watchdog_init(&q->watchdog, sch);
-	q->qdisc = &noop_qdisc;
+	hull_params_init(&q->params);
+	hull_vars_init(&q->vars);
+	sch->limit = q->params.limit;
 
-	if (!opt)
-		return -EINVAL;
+	q->sch = sch;
 
-	hull_params_init(q);
-	hull_vars_init(q);
-	q->vars.mtu = psched_mtu(qdisc_dev(sch));
+	if (opt) {
+		err = hull_change(sch, opt, extack);
+		if (err)
+			return err;
+	}
 
-	return hull_change(sch, opt, extack);
+	return 0;
 }
-
 
 static int hull_dump(struct Qdisc *sch, struct sk_buff *skb)
 {
 	struct hull_sched_data *q = qdisc_priv(sch);
 	struct nlattr *opts = nla_nest_start_noflag(skb, TCA_OPTIONS);
 
-	sch->qstats.backlog = q->qdisc->qstats.backlog;
 	if (!opts)
 		goto nla_put_failure;
 
 	if (nla_put_u32(skb, TCA_HULL_LIMIT, sch->limit) ||
-	    nla_put_u32(skb, TCA_HULL_DRATE, q->params.drate) ||
+	    nla_put_u32(skb, TCA_HULL_DRATE, q->params.drate * MSEC_PER_SEC) ||
 	    nla_put_u32(skb, TCA_HULL_MARKTH, q->params.markth))
 		goto nla_put_failure;
 
@@ -305,8 +210,8 @@ static int hull_dump_stats(struct Qdisc *sch, struct gnet_dump *d)
 {
 	struct hull_sched_data *q = qdisc_priv(sch);
 	struct tc_hull_xstats st = {
+		.qdelay         = q->stats.qdelay,
 		.avg_rate	= q->stats.avg_rate,
-		.qdelay 	= q->stats.qdelay,
 		.packets_in	= q->stats.packets_in,
 		.dropped	= q->stats.dropped,
 		.overlimit	= q->stats.overlimit,
@@ -317,101 +222,43 @@ static int hull_dump_stats(struct Qdisc *sch, struct gnet_dump *d)
 	return gnet_stats_copy_app(d, &st, sizeof(st));
 }
 
+static struct sk_buff *hull_qdisc_dequeue(struct Qdisc *sch)
+{
+	struct hull_sched_data *q = qdisc_priv(sch);
+	u64 qdelay = 0ULL;
+	struct sk_buff *skb = qdisc_dequeue_head(sch);
+
+	if (unlikely(!skb))
+		return NULL;
+
+	/* >> 10 is approx /1000 */
+	qdelay = ((__force __u64)(ktime_get_real_ns() -
+				  ktime_to_ns(skb_get_ktime(skb)))) >> 10;
+	q->stats.qdelay = qdelay;
+
+	return skb;
+}
+
 static void hull_reset(struct Qdisc *sch)
 {
 	struct hull_sched_data *q = qdisc_priv(sch);
 
-	qdisc_reset(q->qdisc);
-	sch->qstats.backlog = 0;
-	sch->q.qlen = 0;
-
-	/* Reset vars */
-	hull_vars_init(q);
-
-	/* Only cancel watchdog if it's been initialized. */
-	if (q->watchdog.qdisc == sch)
-		qdisc_watchdog_cancel(&q->watchdog);
+	qdisc_reset_queue(sch);
+	hull_vars_init(&q->vars);
 }
-
-static void hull_destroy(struct Qdisc *sch)
-{
-	struct hull_sched_data *q = qdisc_priv(sch);
-
-	/* Only cancel watchdog if it's been initialized. */
-	if (q->watchdog.qdisc == sch)
-		qdisc_watchdog_cancel(&q->watchdog);
-	qdisc_put(q->qdisc);
-}
-
-static int hull_dump_class(struct Qdisc *sch, unsigned long cl,
-			   struct sk_buff *skb, struct tcmsg *tcm)
-{
-	struct hull_sched_data *q = qdisc_priv(sch);
-
-	tcm->tcm_handle |= TC_H_MIN(1);
-	tcm->tcm_info = q->qdisc->handle;
-
-	return 0;
-}
-
-static int hull_graft(struct Qdisc *sch, unsigned long arg, struct Qdisc *new,
-		      struct Qdisc **old, struct netlink_ext_ack *extack)
-{
-	struct hull_sched_data *q = qdisc_priv(sch);
-
-	if (new == NULL)
-		new = &noop_qdisc;
-
-	*old = qdisc_replace(sch, new, &q->qdisc);
-	return 0;
-}
-
-static struct Qdisc *hull_leaf(struct Qdisc *sch, unsigned long arg)
-{
-	struct hull_sched_data *q = qdisc_priv(sch);
-	return q->qdisc;
-}
-
-static unsigned long hull_find(struct Qdisc *sch, u32 classid)
-{
-	return 1;
-}
-
-static void hull_walk(struct Qdisc *sch, struct qdisc_walker *walker)
-{
-	if (!walker->stop) {
-		if (walker->count >= walker->skip)
-			if (walker->fn(sch, 1, walker) < 0) {
-				walker->stop = 1;
-				return;
-			}
-		walker->count++;
-	}
-}
-
-static const struct Qdisc_class_ops hull_class_ops = {
-	.graft		=	hull_graft,
-	.leaf		=	hull_leaf,
-	.find		=	hull_find,
-	.walk		=	hull_walk,
-	.dump		=	hull_dump_class,
-};
 
 static struct Qdisc_ops hull_qdisc_ops __read_mostly = {
-	.next		=	NULL,
-	.cl_ops		=	&hull_class_ops,
-	.id		=	"hull",
-	.priv_size	=	sizeof(struct hull_sched_data),
-	.enqueue	=	hull_enqueue,
-	.dequeue	=	hull_dequeue,
-	.peek		=	qdisc_peek_dequeued,
-	.init		=	hull_init,
-	.reset		=	hull_reset,
-	.destroy	=	hull_destroy,
-	.change		=	hull_change,
-	.dump		=	hull_dump,
-	.dump_stats	=	hull_dump_stats,
-	.owner		=	THIS_MODULE,
+	.id		= "hull",
+	.priv_size	= sizeof(struct hull_sched_data),
+	.enqueue	= hull_qdisc_enqueue,
+	.dequeue	= hull_qdisc_dequeue,
+	.peek		= qdisc_peek_dequeued,
+	.init		= hull_init,
+	.reset		= hull_reset,
+	.change		= hull_change,
+	.dump		= hull_dump,
+	.dump_stats	= hull_dump_stats,
+	.owner		= THIS_MODULE,
 };
 
 static int __init hull_module_init(void)
@@ -424,7 +271,9 @@ static void __exit hull_module_exit(void)
 	unregister_qdisc(&hull_qdisc_ops);
 }
 
-module_init(hull_module_init)
-module_exit(hull_module_exit)
+module_init(hull_module_init);
+module_exit(hull_module_exit);
 
+MODULE_DESCRIPTION("PQ scheduler");
+MODULE_AUTHOR("Kr1stj0n C1k0");
 MODULE_LICENSE("GPL");
